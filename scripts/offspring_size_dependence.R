211ae8ff8521f8e07093f268d332dfee859212fc
sam per <prnsml@gmail.com>
2020-04-27 10:11:43 +0200

    correlation adjusted means between thick and weight

--- scripts/offspring_size_dependence.R
+++ scripts/offspring_size_dependence.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 
-.packages = c("optparse", "dplyr", "tidyr", "knitr", "kableExtra", "ggrepel",
+.packages = c("optparse", "dplyr", "tidyr", "knitr", "kableExtra", "ggrepel", "RColorBrewer",
               "flextable", "officer", "ggcorrplot", "data.table", "car", "MASS", "ggplot2")
 # Install CRAN packages (if not already installed)
 .inst <- .packages %in% installed.packages()
@@ -67,13 +67,78 @@ table(mod_dat[mod_dat$generation==0, "maturity"])
 table(mod_dat[mod_dat$generation==1, "maturity"])
 rm(list = c("tbl1", "scaled_phen"))
 mod_dat = as.data.frame(mod_dat)
+head(mod_dat)
+
+# T_dat = mod_dat
+# rm(list=setdiff(ls(), "T_dat"))
+# W_dat = mod_dat
+# head(T_dat)
+# head(W_dat)
+# table(T_dat$pop)
+# table(W_dat$pop)
+# rm(mod_dat)
+
+## model all samples combined
+col_genx = c("#5e3c99", "#e66101")
+frmls = c(paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * pop * generation"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * pop"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * generation"))
+# times_pop_gen.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * pop * generation, data = mod_dat)
+# times_pop_all.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * pop, data = mod_dat)
+# times_gen_all.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * generation, data = mod_dat)
+times_pop_gen.lm = lm(formula = frmls[1], data = mod_dat)
+times_pop_all.lm = lm(formula = frmls[2], data = mod_dat)
+times_gen_all.lm = lm(formula = frmls[3], data = mod_dat)
+(aic.low = frmls[which.min(c(AIC(times_pop_gen.lm),
+                             AIC(times_pop_all.lm),
+                             AIC(times_gen_all.lm)))])
+
+# plot best fit (model with the lowest AIC) onto the data
+best_plot <- ggplot(mod_dat, aes_string(x = paste0('scaled_', one_phen[2]), y = paste0('scaled_', one_phen[1]))) +
+  labs(y = paste0('scaled_', one_phen[1]), x = paste0('scaled_', one_phen[2]),
+       title = paste0("All samples combined - lowest AIC: ", aic.low)) +
+  theme(panel.background = element_blank(),
+        strip.background=element_rect(fill="#91bfdb"),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+        axis.line = element_line(size = 0.2, linetype = "solid",
+                                 colour = "black"),
+        panel.grid = element_line(colour = "gray70", size = 0.2),
+        axis.title = element_text(size = 17),
+        axis.text = element_text(size = 13),
+        legend.text = element_text(size = 14), legend.title = element_text(size = 14),
+        title = element_text(size = 12))
+(fit_plot = best_plot +
+    geom_smooth(method = "lm", se = TRUE, aes(col = factor(generation), fill = factor(generation)), alpha = 0.4) +
+    geom_point(aes(col = factor(generation)), alpha = 0.6) +
+    scale_fill_manual(values = col_genx) +
+    scale_color_manual(values = col_genx) +
+    labs(col = "generation", fill = "generation"))
+# save figures
+dir.create(paste0(dirname(dat_dir), "/", island, "_off_final_figures"))
+ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_best_lm_",
+                   paste(one_phen, collapse = "_"), "_all_samples.svg"),
+       plot=fit_plot, width=10, height=8)
+ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_best_lm_",
+                   paste(one_phen, collapse = "_"), "_all_samples.pdf"),
+       plot=fit_plot, width=10, height=8)
+
+## model with separate generations
 # change the next variable with either 0 or 1
-one_gen = 1
+one_gen = 0
 one_dat = mod_dat[mod_dat$generation==one_gen, ]
 # reminder about which phenotypes we are analysing
 colnames(mod_dat)[col_phen]
 
-basic.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm, data = one_dat)
+frmls = c(paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2]),
+          paste0("scaled_", one_phen[1], " ~ pop"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " + pop"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " + maturity"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " + maturity + pop"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * pop"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * maturity"),
+          paste0("scaled_", one_phen[1], " ~ scaled_", one_phen[2], " * maturity * pop"))
+
+basic.lm = lm(formula = frmls[1], data = one_dat)
 
 # plot(basic.lm[[1]], which = 1)
 # plot(basic.lm[[2]], which = 1)
@@ -81,34 +146,39 @@ basic.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm, data = one_dat)
 # plot(basic.lm[[2]], which = 2)
 # lapply(basic.lm, summary)
 
-bypop.lm = lm(scaled_weight_cuberoot ~ pop, data = one_dat)
+bypop.lm = lm(formula = frmls[2], data = one_dat)
 
-add_pop.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm + pop, data = one_dat)
+add_pop.lm = lm(formula = frmls[3], data = one_dat)
 
-add_mat.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm + maturity, data = one_dat)
+add_mat.lm = lm(formula = frmls[4], data = one_dat)
 
-add_mat_pop.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm + maturity + pop, data = one_dat)
+add_mat_pop.lm = lm(formula = frmls[5], data = one_dat)
 
-times_pop.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * pop, data = one_dat)
+times_pop.lm = lm(formula = frmls[6], data = one_dat)
 
-times_mat.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * maturity, data = one_dat)
+times_mat.lm = lm(formula = frmls[7], data = one_dat)
 
-times_mat_pop.lm = lm(scaled_weight_cuberoot ~ scaled_size_mm * maturity * pop, data = one_dat)
+times_mat_pop.lm = lm(formula = frmls[8], data = one_dat)
 
-aic.lm = as.data.frame(rbind(c("weight ~ size", AIC(basic.lm)),
-                             c("weight ~ population", AIC(bypop.lm)),
-                             c("weight ~ size + population", AIC(add_pop.lm)),
-                             c("weight ~ size + maturity", AIC(add_mat.lm)),
-                             c("weight ~ size + maturity + population", AIC(add_mat_pop.lm)),
-                             c("weight ~ size x population", AIC(times_pop.lm)),
-                             c("weight ~ size x maturity", AIC(times_mat.lm)),
-                             c("weight ~ size x maturity x population", AIC(times_mat_pop.lm))))
+aic.lm = as.data.frame(rbind(c(frmls[1], AIC(basic.lm)),
+                             c(frmls[2], AIC(bypop.lm)),
+                             c(frmls[3], AIC(add_pop.lm)),
+                             c(frmls[4], AIC(add_mat.lm)),
+                             c(frmls[5], AIC(add_mat_pop.lm)),
+                             c(frmls[6], AIC(times_pop.lm)),
+                             c(frmls[7], AIC(times_mat.lm)),
+                             c(frmls[8], AIC(times_mat_pop.lm))))
 colnames(aic.lm) = c("formula", paste0("AIC_gen", one_gen))
 aic.lm
 (aic.low = as.character(aic.lm[which.min(as.numeric(as.character(aic.lm[, 2]))), "formula"]))
+aic.lm[, paste0("AIC_gen", one_gen)] = round(as.numeric(as.character(aic.lm[, paste0("AIC_gen", one_gen)])), 2)
+aic.lm[order(aic.lm[, paste0("AIC_gen", one_gen)]),]
 
+col_mat = c("#1b9e77", "#e7298a")
+# define which variable to use for plot colours (e.g., "pop" or "maturity")
+col_mod = "pop"
 # plot best fit (model with the lowest AIC) onto the data
-best_plot <- ggplot(one_dat, aes(x = scaled_size_mm, y = scaled_weight_cuberoot)) +
+best_plot <- ggplot(one_dat, aes_string(x = paste0('scaled_', one_phen[2]), y = paste0('scaled_', one_phen[1]))) +
   labs(y = paste0('scaled_', one_phen[1]), x = paste0('scaled_', one_phen[2]),
        title = paste0('generation ', one_gen, " - lowest AIC: ", aic.low)) +
   theme(panel.background = element_blank(),
@@ -117,13 +187,20 @@ best_plot <- ggplot(one_dat, aes(x = scaled_size_mm, y = scaled_weight_cuberoot)
         axis.line = element_line(size = 0.2, linetype = "solid",
                                  colour = "black"),
         panel.grid = element_line(colour = "gray70", size = 0.2),
-        axis.title = element_text(size = 14),
-        axis.text = element_text(size = 11))
+        axis.title = element_text(size = 17),
+        axis.text = element_text(size = 9),
+        legend.text = element_text(size = 14), legend.title = element_text(size = 14),
+        title = element_text(size = 13))
 
 (fit_plot = best_plot +
-    geom_smooth(method = "lm", se = TRUE, aes(col = pop, fill = pop), alpha = 0.2) +
-    geom_point(aes(col = pop)))
-
+    # only if col_mod is "maturity"
+    # facet_wrap(col_mod) +
+    geom_smooth(method = "lm", se = TRUE, aes_string(col = col_mod, fill = col_mod), alpha = 0.2) +
+    geom_point(aes_string(col = col_mod), alpha = 0.5))
+    # only if col_mod is "maturity"
+    # scale_color_manual(values = col_mat) + scale_fill_manual(values = col_mat))
+# summary(add_mat_pop.lm)
+# summary(times_mat_pop.lm)
 # save figures
 dir.create(paste0(dirname(dat_dir), "/", island, "_off_final_figures"))
 ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_best_lm_",
@@ -133,18 +210,30 @@ ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island,
                    paste(one_phen, collapse = "_"), "_gen", one_gen, ".pdf"),
        plot=fit_plot, width=10, height=8)
 
-# predict weight at the overall mean size
+# predict phenotype at the overall mean size
 aic.low
-summary(times_pop.lm)
-one_pred = data.frame(predict(times_pop.lm, data.frame(scaled_size_mm = mean(mod_dat$scaled_size_mm),
-                                                       pop = unique(as.character(one_dat$pop))), interval = "confidence"),
-                      pop = unique(one_dat$pop),
-                      generation = one_gen)
+# summary(times_pop.lm)
+if (grepl(pattern = "maturity", x = aic.low)) {
+  one_pred = data.frame(predict(add_mat_pop.lm, data.frame(scaled_size_mm = mean(mod_dat$scaled_size_mm),
+                                                           maturity = c(rep("juvenile", length(unique(as.character(one_dat$pop)))),
+                                                                        rep("adult", length(unique(as.character(one_dat$pop))))),
+                                                           pop = rep(unique(as.character(one_dat$pop)), 2)), interval = "confidence"),
+                        maturity = c(rep("juvenile", length(unique(one_dat$pop))), rep("adult", length(unique(as.character(one_dat$pop))))),
+                        pop = rep(unique(one_dat$pop), 2),
+                        generation = one_gen)
+} else {
+  one_pred = data.frame(predict(times_pop.lm, data.frame(scaled_size_mm = mean(mod_dat$scaled_size_mm),
+                                                         pop = unique(as.character(one_dat$pop))), interval = "confidence"),
+                        pop = unique(one_dat$pop),
+                        generation = one_gen)
+}
+
 # test for the function predict
 coef(times_pop.lm)
 coef(times_pop.lm)[1] + coef(times_pop.lm)[2] * mean(mod_dat$scaled_size_mm)
 coef(times_pop.lm)[1] + coef(times_pop.lm)[2] * mean(mod_dat$scaled_size_mm) + coef(times_pop.lm)[3] + coef(times_pop.lm)[19] * mean(mod_dat$scaled_size_mm)
 coef(times_pop.lm)[1] + coef(times_pop.lm)[2] * mean(mod_dat$scaled_size_mm) + coef(times_pop.lm)[4] + coef(times_pop.lm)[20] * mean(mod_dat$scaled_size_mm)
+###############################
 
 dir.create(paste0(dirname(dat_dir), "/", island, "_off_results"))
 res_dir = paste0(dirname(dat_dir), "/", island, "_off_results")
@@ -158,7 +247,7 @@ if (file.exists(paste0(res_dir, "/tables/", island, "_predict_mean_", one_phen[1
               quote = FALSE, row.names = FALSE, append = TRUE, col.names = TRUE, sep = ",")
 }
 
-# scatterplot lab vs wild predicted weight
+# scatterplot lab vs wild predicted phenotype
 pred_dat = read.csv(file = paste0(res_dir, "/tables/", island, "_predict_mean_", one_phen[1], ".csv"))
 col_genx = c("#5e3c99", "#e66101")
 ggplot(data = pred_dat[pred_dat$generation==0 & pred_dat$pop!="A", ]) +
@@ -249,3 +338,104 @@ ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island,
 ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_boxplot_scaled_", one_phen[1], ".pdf"),
        plot=e2, width=10, height=8)
 
+## weight variation explained by thickness
+# correlation of the adjusted means and regression coefficients
+# using the model with the interactions between size and populations
+# (i.e., best model for weight but not for thickness)
+two_phen = c(off_phen[2], off_phen[3], "size_mm")
+one_gen = 0
+T_dat$snail_ID = paste(T_dat$pop, T_dat$ID, sep = "_")
+W_dat$snail_ID = paste(W_dat$pop, W_dat$ID, sep = "_")
+rm(mod_dat)
+mod_dat = merge(W_dat, T_dat)
+head(mod_dat)
+
+one_dat = mod_dat[mod_dat$generation==one_gen, ]
+
+frmls = c(paste0("scaled_", two_phen[1], " ~ scaled_", two_phen[3], " * pop"),
+          paste0("scaled_", two_phen[2], " ~ scaled_", two_phen[3], " * pop"))
+
+T_times_pop.lm = lm(formula = frmls[1], data = one_dat)
+W_times_pop.lm = lm(formula = frmls[2], data = one_dat)
+
+summary(T_times_pop.lm)
+summary(W_times_pop.lm)
+
+# correlation of slopes
+regr_idx = grepl(pattern = paste0('scaled_', two_phen[3]), x = names(coef(T_times_pop.lm)))
+(regr_coef = data.frame(thick_slopes = round(coef(T_times_pop.lm)[regr_idx], 2),
+                        weight_slopes = round(coef(W_times_pop.lm)[regr_idx], 2)))
+round(cor(regr_coef$thick_slopes, regr_coef$weight_slopes), 2)
+# correlation of slopes + intercepts
+(all_coef = data.frame(thickness = round(coef(T_times_pop.lm), 2),
+                       weight = round(coef(W_times_pop.lm), 2)))
+round(cor(all_coef$thickness, all_coef$weight), 2)
+
+# correlation adjusted means
+one_pred = rbind(data.frame(round(predict(T_times_pop.lm,
+                                          data.frame(scaled_size_mm = mean(mod_dat$scaled_size_mm),
+                                                     pop = unique(as.character(one_dat$pop))), interval = "confidence"), 2),
+                            pop = unique(one_dat$pop),
+                            generation = one_gen,
+                            phenotype = two_phen[1]),
+                 data.frame(round(predict(W_times_pop.lm,
+                                          data.frame(scaled_size_mm = mean(mod_dat$scaled_size_mm),
+                                                     pop = unique(as.character(one_dat$pop))), interval = "confidence"), 2),
+                            pop = unique(one_dat$pop),
+                            generation = one_gen,
+                            phenotype = two_phen[2]))
+one_pred = one_pred[order(one_pred$pop), ]
+round(cor(one_pred[one_pred$phenotype==two_phen[1], "fit"],
+          one_pred[one_pred$phenotype==two_phen[2], "fit"]), 2)
+
+# display.brewer.all(colorblindFriendly = TRUE)
+# display.brewer.pal(n = 8, name = 'Set2')
+# brewer.pal(n = 8, name = 'Set2')
+# display.brewer.pal(n = 8, name = 'Paired')
+# brewer.pal(n = 8, name = 'Paired')
+
+# boxplot two phenotypes adjusted means
+(pred_plot = ggplot(data = one_pred, aes(x = pop, y = fit, col = factor(phenotype))) +
+    geom_point(position = position_dodge(0.9), size = 3) +
+    geom_errorbar(aes(ymin = lwr, ymax = upr), size = 0.5, position = position_dodge(0.9)) +
+    scale_color_manual(values = c("#1F78B4", "#B3B3B3")) +
+    labs(x = "", y = "Size-adjusted means", col = "", title = paste0("Generation ", one_gen)) +
+    theme(legend.position = "top", legend.text = element_text(size = 11), legend.title = element_text(size = 14),
+          plot.title = element_text(size = 14, hjust = 0.5),
+          axis.title.y = element_text(size = 14),
+          axis.text = element_text(size = 11),
+          # axis.ticks = element_line(size = 0.5),
+          panel.background = element_blank(),
+          strip.background=element_rect(fill="#91bfdb"),
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+          axis.line = element_line(size = 0.2, linetype = "solid",
+                                   colour = "black"),
+          panel.grid = element_line(colour = "gray70", size = 0.2)))
+ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_boxplot_adjusted_", paste(two_phen[-3], collapse = "_"),
+                   "_gen",one_gen, ".svg"),
+       plot=pred_plot, width=10, height=8)
+ggsave(file=paste0(dirname(dat_dir), "/", island, "_off_final_figures/", island, "_off_boxplot_adjusted_", paste(two_phen[-3], collapse = "_"),
+                   "_gen",one_gen, ".pdf"),
+       plot=pred_plot, width=10, height=8)
+
+col_mod = "pop"
+# plot best fit (model with the lowest AIC) onto the data
+best_plot <- ggplot(one_dat, aes_string(x = paste0('scaled_', one_phen[2]), y = paste0('scaled_', one_phen[1]))) +
+  labs(y = paste0('scaled_', one_phen[1]), x = paste0('scaled_', one_phen[2]),
+       title = paste0('generation ', one_gen, " - lowest AIC: ", aic.low)) +
+  theme(panel.background = element_blank(),
+        strip.background=element_rect(fill="#91bfdb"),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+        axis.line = element_line(size = 0.2, linetype = "solid",
+                                 colour = "black"),
+        panel.grid = element_line(colour = "gray70", size = 0.2),
+        axis.title = element_text(size = 17),
+        axis.text = element_text(size = 9),
+        legend.text = element_text(size = 14), legend.title = element_text(size = 14),
+        title = element_text(size = 13))
+
+(fit_plot = best_plot +
+    # only if col_mod is "maturity"
+    # facet_wrap(col_mod) +
+    geom_smooth(method = "lm", se = TRUE, aes_string(col = col_mod, fill = col_mod), alpha = 0.2) +
+    geom_point(aes_string(col = col_mod), alpha = 0.5))
